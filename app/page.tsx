06caa9792b9dc356d84775d3e66682afcf5abb77
MOLOORD232 <Mdmdijmzjwjf@gmail.com>
2025-01-07 01:11:49 +0200

    Create page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+"use client"
+import QuizApp from '../components/QuizApp'
+
+export default function Home() {
+  return (
+    <main className="min-h-screen p-4">
+      <QuizApp />
+    </main>
+  )
+}
